cded7a7defd0dd299537e01e9dd0696441cea3de
JITENDRA-BARIK <jitendrabarik223@gmail.com>
2026-06-30 00:53:56 +0530

    chore: run checks, fix issues, update frontend

--- hospital_management/wsgi.py
+++ hospital_management/wsgi.py
@@ -5,3 +5,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hospital_management.settings")
 
 application = get_wsgi_application()
+git push -u origin <branch>git fetch origin
+git pull --rebase origin <branch>
+# resolve conflicts if any, then:
+git push origin <branch>
